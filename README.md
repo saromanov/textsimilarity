29952b38765cd55d51b4c90bc6212467a088fc13
Sergey Romanov <xxsmotur@gmail.com>
2015-06-07 02:13:51 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# textsimilarity
+
+## Install
+
+## Usage
+
+```javascript
+var textsimilarity = require('textsimilarity');
+console.log(textsimilarity("It was a great", "It was a fun")); //0.75
+```
+
+## LICENSE
+MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+var math = require('mathjs');
+
+module.exports = function(text1, text2){
+	if(text1.length == 0) {
+		return 0;
+	}
+
+	if(text2.length == 0) {
+		return 0;
+	}
+	text1 += ' ';
+	text1 = Prepare(text1);
+	text2 = Prepare(text2);
+	var total = text1.concat(text2).split(' ');
+	var allwords = total.filter(function(word, pos){
+		return total.indexOf(word) == pos;
+	});
+	var arr1 = TextToVector(text1, allwords);
+	var arr2 = TextToVector(text2, allwords);
+	return math.dot(arr1, arr2)/(math.norm(arr1) * math.norm(arr2))
+}
+
+var Prepare = function(text){
+	return text.toLowerCase();
+}
+
+var TextToVector = function(text, bagofwords){
+	return bagofwords.map(function(word){
+		var totallength = text.split(' ').filter(function(x){ return x == word;}).length;
+		return text.split(' ').filter(function(x){ return x == word;}).length;
+	});
+};
